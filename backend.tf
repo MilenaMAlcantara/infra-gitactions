2a2a79e3df7ce291b698ff7cc96d375c6cde1c3d
milena.alcantara <milenamesquita.alcantara@gmail.com>
2025-11-10 03:33:52 +0000

    init repo

---
+++ backend.tf
@@ -0,0 +1,10 @@
+# state.tf
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-almilena"
+    key     = "site/terraform.tfstate"
+    region  = "sa-east-1"
+    encrypt = true
+
+  }
+}
---
+++ ec2.tf
@@ -0,0 +1,55 @@
+resource "aws_instance" "website_server" {
+  ami                    = "ami-0f09890f797d40fb4"
+  instance_type          = "t2.micro"
+  key_name               = "chave-site-prod"
+  vpc_security_group_ids = [aws_security_group.website_sg.id]
+  iam_instance_profile   = "ECR-EC2-ROLE"
+
+  tags = {
+    Name        = "website-server"
+    Provisioned = "Terraform"
+  }
+
+  depends_on = [ aws_security_group.website_sg ]
+}
+
+resource "aws_security_group" "website_sg" {
+  name   = "website-sg"
+  vpc_id = "vpc-090d8050c5096a982"
+
+  tags = {
+    Name        = "website_sg"
+    Provisioned = "Terraform"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.website_sg.id
+  cidr_ipv4         = "177.196.63.11/32"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.website_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.website_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.website_sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = -1
+}
