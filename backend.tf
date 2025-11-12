764b127af038b04f2528f3376e191d101e02b42d
milena.alcantara <milenamesquita.alcantara@gmail.com>
2025-11-12 01:27:20 +0000

    add git actions

--- backend.tf
+++ backend.tf
@@ -1,10 +1,11 @@
 # state.tf
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-almilena"
-    key     = "site/terraform.tfstate"
-    region  = "sa-east-1"
-    encrypt = true
+    bucket       = "terraform-state-almilena"
+    key          = "site/terraform.tfstate"
+    region       = "sa-east-1"
+    encrypt      = true
+    use_lockfile = true
 
   }
 }
